ee07a1bf673335c0ab6d99944779077465d6eb95
Hakan <143508175+KebanFiru@users.noreply.github.com>
2024-09-02 15:52:13 +0300

    Ability to add second number added

--- src/main.js
+++ src/main.js
@@ -1,33 +1,48 @@
 let firstNumber;
+let secondNumber;
+let number;
 let operator = false;
+let operatorType;
+let isfirstnumberwrote = false;
 
 const buttons = document.querySelectorAll("button");
 const display = document.getElementById("ResultText");
 
 buttons.forEach((button) => {
-    button.addEventListener("click", () =>{
-        buttonValue = button.textContent;
-        if (button.classList == "NumberButton") {
-            firstNumber = display.textContent += buttonValue;
-            if(!operator ){         
-                console.log(operator);       
-                firstNumber
-            }
-            else{
-                operator = false;
-                display.textContent = '';
+  button.addEventListener("click", () => {
+    if(button.classList == "NumberButton"){
+        number = display.textContent += button.textContent;
+        if(!isfirstnumberwrote) {
+          if (!operator) {
+            firstNumber = number;
+          } else {
+            operator = false;
+            display.textContent = "";
+          }
+        }
+        else{
+            if (!operator) {
+              secondNumber = number;
+            } else {
+              operator = false;
+              display.textContent = "";
             }
         }
-        else if (button.classlist = "OperatorButton"){
-            if(button.textContent == "Clear"){ 
-                display.textContent = '';
-            }
-            else {
-                operator = true;
-                display.textContent = '';
-                display.textContent = button.textContent;
-            }
-            
+    }
+    else if(button.classList == "OperatorButton"){
+        if(button.textContent == "Clear"){
+            display.textContent = '';
         }
-    });
+        else if(button.textContent == "="){
+            console.log(firstNumber + secondNumber);
+        }
+        else{
+            operator = true;
+            isfirstnumberwrote = true;
+            display.textContent = '';
+            display.textContent = button.textContent;
+
+        }
+    }
+  });
 });
